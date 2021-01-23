90ac6dc6dbf336f5d9600ed33df3e411eceafd16
EC2 Default User <ec2-user@ip-172-31-9-86.ap-northeast-1.compute.internal>
2021-01-23 17:58:40 +0000

    ログの処理を追加

--- app.py
+++ app.py
@@ -3,31 +3,67 @@ import ccxt
 import time
 import os
 import setting
+import math
+import logging
+import logging.handlers
 
 app = Flask(__name__)
+
 coincheck = ccxt.coincheck()
-coincheck.apiKey = os.environ.get("COINCHECK_APIKEY")
-coincheck.secret = os.environ.get("COINCHECK_SECRET")
+coincheck.apiKey = os.environ.get("APIKEY")
+coincheck.secret = os.environ.get("SECRET")
+
+@app.route('/')
+def index():
+    return "paibot is running..."
 
 @app.route('/webhook',methods=['POST'])
 def webhook():
     if request.method == 'POST':
         message = request.get_data(as_text=True)
+        app.logger.info("webhook message:" + message)
+
         balance = coincheck.fetchBalance()["info"]
-        #1秒に2回以上認証が必要なリクエストが発行できないため1秒停止
-        time.sleep(1)
-        balance_jpy = round(float(balance["jpy"]))
+        balance_jpy = math.floor(float(balance["jpy"]))
         balance_btc = round(float(balance["btc"]),8)
-        if message in "buy":
-            coincheck.create_market_buy_order('BTC/JPY',balance_jpy)
+
+        #Nonceを1つ増加させるため1秒停止
+        time.sleep(1)
+
+        if "buy" in message:
+            order = coincheck.create_market_buy_order('BTC/JPY',balance_jpy)
+            app.logger.info("order id: %s %s %s",order['info']['id'] , order['info']['order_type'] , order['info']['amount'])
             return '',200           
-        elif message in "sell":
-            coincheck.create_market_buy_order('BTC/JPY',balance_btc)
+        elif "sell" in message:
+            order = coincheck.create_market_sell_order('BTC/JPY',balance_btc)
+            app.logger.info("order id: %s %s %s",order['info']['id'] , order['info']['order_type'] , order['info']['market_buy_amount'])
             return '',200
         else:
             abort(400)
     else:
         abort(403)
 
+@app.errorhandler(Exception)
+def server_error(err):
+    app.logger.exception(err)
+    return '', 500
+
 if __name__ == '__main__':
+    
+    #ログ設定
+    app.logger.setLevel(logging.DEBUG)
+    
+    #アプリケーションログ
+    handler = logging.handlers.RotatingFileHandler("app.log", maxBytes=100000, backupCount=10)
+    handler.setLevel(logging.DEBUG) 
+    handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s'))
+    app.logger.addHandler(handler)
+
+    #アクセスログ
+    logger = logging.getLogger('werkzeug')
+    handler = logging.handlers.RotatingFileHandler('access.log',maxBytes=100000, backupCount=10)
+    logger.addHandler(handler)
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+    
     app.run(host="0.0.0.0",port="80")
